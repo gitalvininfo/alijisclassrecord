05a9580a2085802a4c40fbb69ba62659880c0317
CarlBetio <carllouiebetio@gmail.com>
2018-07-04 23:40:52 +0800

    updated insert function (error fixed)

--- crud/saveclassrecord.php
+++ crud/saveclassrecord.php
@@ -14,7 +14,7 @@ if(ISSET($_POST['submit'])){
     $teacher_id = $_POST['teacher_id'];
     $subject_name = $_POST['subject_name'];
     $school_year = $_POST['school_year'];
-    
+    echo $school_year;
     $new = "";
 
     $conn = new mysqli("localhost", "root", "", "alijisclassrecord") or die(mysqli_error());
@@ -114,6 +114,6 @@ if(ISSET($_POST['submit'])){
     }
 
     echo "<script type='text/javascript'>alert('Successfully added new written works!');</script>";
-    echo "<script>document.location='../studentslist.php?id=$teacher_id&subject_name=$subject_name'</script>";  
+   // echo "<script>document.location='../studentslist.php?id=$teacher_id&subject_name=$subject_name'</script>";  
 
 ?>
