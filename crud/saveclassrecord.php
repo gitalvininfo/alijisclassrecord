a08a708e7421252571d1967633381508c5db5d0d
Alvin Yanson <yansonalvin29@gmail.com>
2018-07-04 23:19:04 +0800

    test

--- crud/saveclassrecord.php
+++ crud/saveclassrecord.php
@@ -5,14 +5,11 @@ if(ISSET($_POST['submit'])){
     $name = $_POST['name'];
     $teacher_id = $_POST['teacher_id'];
     $subject_name = $_POST['subject_name'];
-    $new = "";
-    $try = "";
+
     foreach(array_combine($w1, $name) as $value => $content)  
     {  
-        $new = $value;
-        $try = $content;
         $conn = new mysqli("localhost", "root", "", "alijisclassrecord") or die(mysqli_error());
-        $conn->query("INSERT INTO `test` VALUES('', '$try', '$new', '$teacher_id', '$subject_name')") or die(mysqli_error());
+        $conn->query("INSERT INTO `test` VALUES('', '$content', '$value', '$teacher_id', '$subject_name')") or die(mysqli_error());
         $conn->close();
     }
 
