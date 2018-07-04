6c3abf8073fd81e1a1abbe3119e43af58fbf9df3
CarlBetio <carllouiebetio@gmail.com>
2018-07-04 16:19:39 +0800

    Updated Create funtion(working)

--- crud/saveclassrecord.php
+++ crud/saveclassrecord.php
@@ -1,24 +1,18 @@
 <?php
-
 if(ISSET($_POST['submit'])){
     $w1 = $_POST["w1"];
     $name = $_POST['name'];
     $teacher_id = $_POST['teacher_id'];
     $subject_name = $_POST['subject_name'];
     $new = "";
-    $try = "";
-    foreach(array_combine($w1, $name) as $value => $content)  
-    {  
-        $new = $value;
-        $try = $content;
-        $conn = new mysqli("localhost", "root", "", "alijisclassrecord") or die(mysqli_error());
-        $conn->query("INSERT INTO `test` VALUES('', '$try', '$new', '$teacher_id', '$subject_name')") or die(mysqli_error());
-        $conn->close();
-    }
-
-
+  
+    $conn = new mysqli("localhost", "root", "", "alijisclassrecord") or die(mysqli_error());
+    foreach(array_combine($name,$w1) as $new => $value){
+    
+        $conn->query("INSERT INTO `test` VALUES('', '$new', '$value', '$teacher_id', '$subject_name')") or die(mysqli_error());
+     }
+    $conn->close();
     echo "<script type='text/javascript'>alert('Successfully added new written works!');</script>";
     echo "<script>document.location='../studentslist.php?id=$teacher_id&subject_name=$subject_name'</script>";  
 }
-
 ?>
